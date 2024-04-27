4d9c8517c53b8ccda505dc0d4fc9c2691eb60cb8
qfc <qfc_dio27@163.com>
2024-04-27 17:20:28 +0800

    feat: 完成layouts布局

---
+++ src/layouts/components/Header/components/CollapseIcon.tsx
@@ -0,0 +1,11 @@
+import { MenuFoldOutlined } from "@ant-design/icons";
+
+const CollapseIcon = () => {
+	return (
+		<div className="collapsed">
+			<MenuFoldOutlined />
+		</div>
+	);
+};
+
+export default CollapseIcon;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "@/App";
 import "@/styles/reset.scss";
+import "@/styles/common.scss";
 import "antd/dist/antd.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
